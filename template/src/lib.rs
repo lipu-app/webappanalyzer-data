7bd3d04d2a051d1b8b7e59701f2c7eb8b00df336
Piggy NL <piggynl@outlook.com>
2024-10-03 09:46:56 +0800

    initial commit

---
+++ template/src/lib.rs
@@ -0,0 +1,33 @@
+pub const CATEGORIES_JSON: &[u8] = include_bytes!("./categories.json");
+
+pub const GROUPS_JSON: &[u8] = include_bytes!("./groups.json");
+
+pub const TECHNOLOGIES_JSONS: [&[u8]; 27] = [
+    include_bytes!("./technologies/_.json"),
+    include_bytes!("./technologies/a.json"),
+    include_bytes!("./technologies/b.json"),
+    include_bytes!("./technologies/c.json"),
+    include_bytes!("./technologies/d.json"),
+    include_bytes!("./technologies/e.json"),
+    include_bytes!("./technologies/f.json"),
+    include_bytes!("./technologies/g.json"),
+    include_bytes!("./technologies/h.json"),
+    include_bytes!("./technologies/i.json"),
+    include_bytes!("./technologies/j.json"),
+    include_bytes!("./technologies/k.json"),
+    include_bytes!("./technologies/l.json"),
+    include_bytes!("./technologies/m.json"),
+    include_bytes!("./technologies/n.json"),
+    include_bytes!("./technologies/o.json"),
+    include_bytes!("./technologies/p.json"),
+    include_bytes!("./technologies/q.json"),
+    include_bytes!("./technologies/r.json"),
+    include_bytes!("./technologies/s.json"),
+    include_bytes!("./technologies/t.json"),
+    include_bytes!("./technologies/u.json"),
+    include_bytes!("./technologies/v.json"),
+    include_bytes!("./technologies/w.json"),
+    include_bytes!("./technologies/x.json"),
+    include_bytes!("./technologies/y.json"),
+    include_bytes!("./technologies/z.json"),
+];
